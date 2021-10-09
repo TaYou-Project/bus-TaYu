8b4d4609fd83fc98b920897d3026e25be0463f2a
ggarrrrrrg <eun-jii>
2021-10-09 22:02:46 +0900

    firebase testing

---
+++ app/src/main/java/org/devTayu/busTayu/firebase_test.java
@@ -0,0 +1,24 @@
+package org.devTayu.busTayu;
+
+import android.os.Bundle;
+import android.os.PersistableBundle;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.firebase.analytics.FirebaseAnalytics;
+
+public class firebase_test extends AppCompatActivity {
+
+    private FirebaseAnalytics mFirebaseAnalytics;
+
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState, @Nullable PersistableBundle persistentState) {
+        super.onCreate(savedInstanceState, persistentState);
+
+        mFirebaseAnalytics = FirebaseAnalytics.getInstance(this);
+
+
+    }
+}
